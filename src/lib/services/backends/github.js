cc953238d5e3d70a5405447e3395e2bfd517dcc9
Kohei Yoshino <kohei.yoshino@gmail.com>
2023-08-23 21:14:14 -0400

    Move shared code out of GitHub backend
    In prep for GitLab backend (#22)

--- src/lib/services/backends/github.js
+++ src/lib/services/backends/github.js
@@ -2,6 +2,7 @@ import { getType } from 'mime';
 import { get } from 'svelte/store';
 import { allAssets } from '$lib/services/assets';
 import { authorize, user } from '$lib/services/auth';
+import { createCommitMessage } from '$lib/services/backends/shared/commits';
 import { siteConfig } from '$lib/services/config';
 import { allEntries } from '$lib/services/contents';
 import { createFileList, parseAssetFiles, parseEntryFiles } from '$lib/services/parser';
@@ -261,67 +262,6 @@ const getLastCommitHash = async () => {
   }`);
 
   return oid;
-};
-
-/**
- * Default commit message templates.
- * @see https://decapcms.org/docs/beta-features/#commit-message-templates
- */
-const defaultCommitMessages = {
-  create: 'Create {{collection}} “{{slug}}”',
-  update: 'Update {{collection}} “{{slug}}”',
-  delete: 'Delete {{collection}} “{{slug}}”',
-  uploadMedia: 'Upload “{{path}}”',
-  deleteMedia: 'Delete “{{path}}”',
-  openAuthoring: '{{message}}',
-};
-
-/**
- * Create a Git commit message.
- * @param {SavingFile[] | DeletingFile[]} items Entries or files.
- * @param {object} [options] Options.
- * @param {CommitType} [options.commitType] Git commit type.
- * @param {string} [options.collection] Collection name. Required for entries.
- * @returns {string} Formatted message.
- */
-const createCommitMessage = (items, { commitType = 'update', collection } = {}) => {
-  const { detail: { login = '', name = '' } = {} } = get(user);
-  const [firstSlug] = items.map((item) => item.slug).filter(Boolean);
-  const [firstPath, ...remainingPaths] = items.map(({ path }) => path);
-  const { backend: { commit_messages: customCommitMessages = {} } = {} } = get(siteConfig);
-  /**
-   * @type {string}
-   */
-  let message = customCommitMessages[commitType] || defaultCommitMessages[commitType] || '';
-
-  if (['create', 'update', 'delete'].includes(commitType)) {
-    message = message
-      .replaceAll('{{slug}}', firstSlug)
-      .replaceAll('{{collection}}', collection)
-      .replaceAll('{{path}}', firstPath)
-      .replaceAll('{{author-login}}', login)
-      .replaceAll('{{author-name}}', name);
-  }
-
-  if (['uploadMedia', 'deleteMedia'].includes(commitType)) {
-    message = message
-      .replaceAll('{{path}}', firstPath)
-      .replaceAll('{{author-login}}', login)
-      .replaceAll('{{author-name}}', name);
-  }
-
-  if (['openAuthoring'].includes(commitType)) {
-    message = message
-      .replaceAll('{{message}}', commitType)
-      .replaceAll('{{author-login}}', login)
-      .replaceAll('{{author-name}}', name);
-  }
-
-  if (remainingPaths.length) {
-    message += ` +${remainingPaths.length}`;
-  }
-
-  return message;
 };
 
 /**
---
+++ src/lib/services/backends/shared/commits.js
@@ -0,0 +1,64 @@
+import { get } from 'svelte/store';
+import { user } from '$lib/services/auth';
+import { siteConfig } from '$lib/services/config';
+
+/**
+ * Default commit message templates.
+ * @see https://decapcms.org/docs/beta-features/#commit-message-templates
+ */
+const defaultCommitMessages = {
+  create: 'Create {{collection}} “{{slug}}”',
+  update: 'Update {{collection}} “{{slug}}”',
+  delete: 'Delete {{collection}} “{{slug}}”',
+  uploadMedia: 'Upload “{{path}}”',
+  deleteMedia: 'Delete “{{path}}”',
+  openAuthoring: '{{message}}',
+};
+
+/**
+ * Create a Git commit message.
+ * @param {SavingFile[] | DeletingFile[]} items Entries or files.
+ * @param {object} [options] Options.
+ * @param {CommitType} [options.commitType] Git commit type.
+ * @param {string} [options.collection] Collection name. Required for entries.
+ * @returns {string} Formatted message.
+ */
+export const createCommitMessage = (items, { commitType = 'update', collection } = {}) => {
+  const { detail: { login = '', name = '' } = {} } = get(user);
+  const [firstSlug] = items.map((item) => item.slug).filter(Boolean);
+  const [firstPath, ...remainingPaths] = items.map(({ path }) => path);
+  const { backend: { commit_messages: customCommitMessages = {} } = {} } = get(siteConfig);
+  /**
+   * @type {string}
+   */
+  let message = customCommitMessages[commitType] || defaultCommitMessages[commitType] || '';
+
+  if (['create', 'update', 'delete'].includes(commitType)) {
+    message = message
+      .replaceAll('{{slug}}', firstSlug)
+      .replaceAll('{{collection}}', collection)
+      .replaceAll('{{path}}', firstPath)
+      .replaceAll('{{author-login}}', login)
+      .replaceAll('{{author-name}}', name);
+  }
+
+  if (['uploadMedia', 'deleteMedia'].includes(commitType)) {
+    message = message
+      .replaceAll('{{path}}', firstPath)
+      .replaceAll('{{author-login}}', login)
+      .replaceAll('{{author-name}}', name);
+  }
+
+  if (['openAuthoring'].includes(commitType)) {
+    message = message
+      .replaceAll('{{message}}', commitType)
+      .replaceAll('{{author-login}}', login)
+      .replaceAll('{{author-name}}', name);
+  }
+
+  if (remainingPaths.length) {
+    message += ` +${remainingPaths.length}`;
+  }
+
+  return message;
+};
