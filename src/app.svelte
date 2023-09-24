1f0accde0e5a5de04579922240cda4c24991f8db
Kohei Yoshino <kohei.yoshino@gmail.com>
2023-09-24 18:20:02 -0400

    Update Sveltia UI

--- src/app.svelte
+++ src/app.svelte
@@ -15,11 +15,7 @@
 <svelte:head>
   <base target="_blank" />
   <meta name="referrer" content="same-origin" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta name="google" content="notranslate" />
   <link rel="icon" href="data:image/svg+xml;base64,{btoa(SveltiaLogo)}" type="image/svg+xml" />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
   {#if import.meta.env.DEV}
     <!--
       Local development can be done by loading a CMS config file from a separate local dev server.
