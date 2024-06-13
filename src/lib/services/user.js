sveltia/sveltia-cms

commit 08e28e85f2d0b44fcc8c34137fd75a0b36696292

Hide Sign In button while signing in

src/lib/services/user.js
@@ -88,17 +88,19 @@ export const signInAutomatically = async () => {
 
   const _backend = get(backend);
 
-  if (!_user || !_backend) {
-    return;
+  if (_user && _backend) {
+    try {
+      _user = await _backend.signIn({ token: _user.token, auto: true });
+    } catch {
+      //
+    }
   }
 
-  try {
-    _user = await _backend.signIn({ token: _user.token, auto: true });
-  } catch {
+  if (!_user) {
     unauthenticated.set(true);
   }
 
-  if (!_user || get(unauthenticated)) {
+  if (!_user || !_backend) {
     return;
   }
 

src/lib/components/entrance/entrance-page.svelte
@@ -94,7 +94,7 @@
       font-size: var(--sui-font-size-xxx-large);
     }
 
-    .message {
+    :global(.message) {
       margin: 0 0 16px;
       font-size: var(--sui-font-size-large);
       font-weight: normal;

src/lib/components/entrance/sign-in.svelte
@@ -19,7 +19,9 @@
 </script>
 
 <div role="none" class="buttons">
-  {#if $backendName === 'local'}
+  {#if !$unauthenticated}
+    <div role="alert" class="message">{$_('signing_in')}</div>
+  {:else if $backendName === 'local'}
     {#if !('showDirectoryPicker' in window)}
       <div role="alert">
         {$_('unsupported.browser')}

src/lib/locales/en.js
@@ -73,6 +73,7 @@ export const strings = {
   loading_site_data_error: 'There was an error while loading site data.',
   sign_in_with_x: 'Sign In with {service}',
   authorizing: 'Authorizing…',
+  signing_in: 'Signing in…',
   work_with_local_repo: 'Work with Local Repository',
   work_with_local_repo_description:
     'Click the button to select the root directory of the “{repo}” repository.',

src/lib/locales/ja.js
@@ -73,6 +73,7 @@ export const strings = {
   loading_site_data_error: 'サイトデータの読み込み中にエラーが発生しました。',
   sign_in_with_x: '{service} でログイン',
   authorizing: '認証中…',
+  signing_in: 'ログイン中…',
   work_with_local_repo: 'ローカルレポジトリで作業',
   work_with_local_repo_description:
     'ボタンをクリックして「{repo}」レポジトリのルートディレクトリを選択してください。',

commit 7f64bc381a9c2e751b22cdc11f2f6baad14fb023

Fix unexpected error with local backend
Fix #165, fallout from 08e28e85f2d0b44fcc8c34137fd75a0b36696292

src/lib/services/user.js
@@ -92,7 +92,8 @@ export const signInAutomatically = async () => {
     try {
       _user = await _backend.signIn({ token: _user.token, auto: true });
     } catch {
-      //
+      // The local backend may throw if the file handle permission is not given
+      _user = undefined;
     }
   }
 