sveltia/sveltia-cms

commit 7a75e2ecb1e89e700a45caa9e80cb4db0abb9a61

Don’t Ignore `<div id="nc-root">` after CMS `<script>`
As per #202

src/main.js
@@ -45,7 +45,15 @@ const compatibilityURL = 'https://github.com/sveltia/sveltia-cms#compatibility';
  * @see https://decapcms.org/docs/manual-initialization/
  * @see https://decapcms.org/docs/custom-mounting/
  */
-const init = ({ config = {} } = {}) => {
+const init = async ({ config = {} } = {}) => {
+  if (!document.querySelector('#nc-root')) {
+    // A custom mount element (`<div id="nc-root">`) could appear after the CMS `<script>`, so just
+    // wait until the page content is loaded
+    await new Promise((resolve) => {
+      window.addEventListener('DOMContentLoaded', () => resolve(undefined), { once: true });
+    });
+  }
+
   mount(App, {
     target: document.querySelector('#nc-root') ?? document.body,
     props: { config },

commit d1e851cd60ca2cbcebcb218b9d3e4bfe59574087

Fix empty page due to never-resolved `await`
Fix #215, regressed in https://github.com/sveltia/sveltia-cms/commit/7a75e2ecb1e89e700a45caa9e80cb4db0abb9a61

src/main.js
@@ -53,7 +53,7 @@ const init = async ({ config = {} } = {}) => {
 
   initialized = true;
 
-  if (!document.querySelector('#nc-root')) {
+  if (document.readyState === 'loading' && !document.querySelector('#nc-root')) {
     // A custom mount element (`<div id="nc-root">`) could appear after the CMS `<script>`, so just
     // wait until the page content is loaded
     await new Promise((resolve) => {