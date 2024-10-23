94560d56f335afbfb6c2a9c11b393eb446c48ae7
Kohei Yoshino <kohei.yoshino@gmail.com>
2024-10-23 06:04:07 -0400

    Remove unnecessary divider in create menu

--- src/lib/components/global/toolbar/items/create-button.svelte
+++ src/lib/components/global/toolbar/items/create-button.svelte
@@ -5,6 +5,11 @@
   import { goto } from '$lib/services/app/navigation';
   import { showUploadAssetsDialog } from '$lib/services/assets/view';
   import { siteConfig } from '$lib/services/config';
+
+  $: folderCollections = ($siteConfig?.collections ?? []).filter(
+    ({ folder, create = false, hide = false, divider = false }) =>
+      !!folder && create && !hide && !divider,
+  );
 </script>
 
 <MenuButton
@@ -18,27 +23,17 @@
   {/snippet}
   {#snippet popup()}
     <Menu aria-label={$_('create_entry_or_assets')}>
-      {#each $siteConfig?.collections ?? [] as collection (collection.name)}
-        {@const {
-          name,
-          label,
-          label_singular: labelSingular,
-          folder,
-          hide = false,
-          create = false,
-          divider = false,
-        } = collection}
-        {#if folder && !hide && !divider}
+      {#if folderCollections.length}
+        {#each folderCollections as { name, label, label_singular: labelSingular } (name)}
           <MenuItem
             label={labelSingular || label || name}
-            disabled={!create}
             onclick={() => {
               goto(`/collections/${name}/new`);
             }}
           />
-        {/if}
-      {/each}
-      <Divider />
+        {/each}
+        <Divider />
+      {/if}
       <MenuItem
         label={$_('assets')}
         onclick={async () => {
