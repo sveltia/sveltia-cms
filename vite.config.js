ebdaf75cc56de6873511e4796c9be66cd2c34738
Kohei Yoshino <kohei.yoshino@gmail.com>
2025-03-19 19:16:52 -0400

    Improve packaging

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,51 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { cp, readFile, writeFile } from 'fs/promises';
+import { existsSync } from 'fs';
+import { cp, mkdir, readFile, writeFile } from 'fs/promises';
 import path from 'path';
 import { sveltePreprocess } from 'svelte-preprocess';
 import { defineConfig } from 'vite';
+
+/**
+ * Copy essential package files while modifying the `package.json` content.
+ * @returns {import('vite').Plugin} Vite plugin.
+ */
+const copyPackageFiles = () => ({
+  name: 'copy-package-files',
+  buildStart: {
+    async: true,
+    sequential: false,
+    // eslint-disable-next-line jsdoc/require-jsdoc
+    handler: async () => {
+      const packageJson = JSON.parse(await readFile('package.json'));
+
+      // Remove unnecessary properties as we only publish compiled bundles
+      delete packageJson.dependencies;
+      delete packageJson.scripts;
+
+      // Add properties for distribution; paths are relative to `package`
+      Object.assign(packageJson, {
+        files: ['dist'],
+        main: './dist/sveltia-cms.mjs',
+        module: './dist/sveltia-cms.mjs',
+        exports: {
+          '.': {
+            default: './dist/sveltia-cms.mjs',
+          },
+        },
+      });
+
+      if (!existsSync('package')) {
+        await mkdir('package');
+      }
+
+      await Promise.all([
+        writeFile('package/package.json', JSON.stringify(packageJson, null, 2).concat('\n')),
+        cp('LICENSE.txt', 'package/LICENSE.txt'),
+        cp('README.md', 'package/README.md'),
+      ]);
+    },
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -46,25 +89,6 @@ export default defineConfig({
         runes: true,
       },
     }),
-    {
-      name: 'copy-package-files',
-      closeBundle: {
-        async: true,
-        sequential: true,
-        // eslint-disable-next-line jsdoc/require-jsdoc
-        handler: async () => {
-          const packageJson = JSON.parse(await readFile('package.json'));
-
-          // Remove unnecessary properties as we only publish compiled bundles
-          delete packageJson.dependencies;
-          delete packageJson.devDependencies;
-          delete packageJson.scripts;
-
-          await writeFile('package/package.json', JSON.stringify(packageJson, null, 2));
-          await cp('LICENSE.txt', 'package/LICENSE.txt');
-          await cp('README.md', 'package/README.md');
-        },
-      },
-    },
+    copyPackageFiles(),
   ],
 });
