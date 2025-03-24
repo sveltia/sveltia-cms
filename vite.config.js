ed390c9c9a2c33055ec43a579b095891b0127f30
Kohei Yoshino <kohei.yoshino@gmail.com>
2025-03-24 03:30:50 -0400

    Generate TypeScript type declaration files
    Fix #346

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { exec } from 'child_process';
 import { existsSync } from 'fs';
 import { cp, mkdir, readFile, writeFile } from 'fs/promises';
 import path from 'path';
@@ -20,17 +21,26 @@ const copyPackageFiles = () => ({
 
       // Remove unnecessary properties as we only publish compiled bundles
       delete packageJson.dependencies;
-      delete packageJson.devDependencies;
       delete packageJson.scripts;
 
       // Add properties for distribution; paths are relative to `package`
       Object.assign(packageJson, {
+        devDependencies: {
+          // Keep only React type declarations used in the generated `d.ts` files
+          '@types/react': packageJson.devDependencies['@types/react'],
+        },
         files: ['dist'],
         main: './dist/sveltia-cms.mjs',
         module: './dist/sveltia-cms.mjs',
         exports: {
           '.': {
+            types: './main.d.ts',
             default: './dist/sveltia-cms.mjs',
+          },
+        },
+        typesVersions: {
+          '>4.0': {
+            index: ['./main.d.ts'],
           },
         },
       });
@@ -44,6 +54,23 @@ const copyPackageFiles = () => ({
         cp('LICENSE.txt', 'package/LICENSE.txt'),
         cp('README.md', 'package/README.md'),
       ]);
+    },
+  },
+});
+
+/**
+ * Generate TypeScript type declaration files from JSDoc comments.
+ * @returns {import('vite').Plugin} Vite plugin.
+ */
+const generateTypes = () => ({
+  name: 'generate-types',
+  buildStart: {
+    async: false,
+    sequential: false,
+    // eslint-disable-next-line jsdoc/require-jsdoc
+    handler: () => {
+      // Produce `main.d.ts` and `types/public.d.ts`
+      exec('tsc src/lib/main.js --allowJs --declaration --emitDeclarationOnly --outDir package');
     },
   },
 });
@@ -91,5 +118,6 @@ export default defineConfig({
       },
     }),
     copyPackageFiles(),
+    generateTypes(),
   ],
 });
