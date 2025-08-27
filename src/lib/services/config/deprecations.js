149e3aba0eccda3225cffc87ba67df77f7d76c2a
Kohei Yoshino <kohei.yoshino@gmail.com>
2025-08-26 23:53:42 -0400

    Clarify deprecation warning messages

--- src/lib/services/config/deprecations.js
+++ src/lib/services/config/deprecations.js
@@ -17,19 +17,20 @@ const warnedOnceMap = {
 const warningMessages = {
   yaml_quote:
     'The `yaml_quote` collection option is deprecated and will be removed in Sveltia CMS 1.0. ' +
-    'Use the global `output.yaml.quote` option instead. ' +
-    'https://github.com/sveltia/sveltia-cms#controlling-data-output',
+    'Use the global `output.yaml.quote` option instead. `yaml_quote: true` is equivalent to ' +
+    '`quote: double`. https://github.com/sveltia/sveltia-cms#controlling-data-output',
   uuid_read_only:
     'The `read_only` option for the UUID widget is deprecated and will be removed in Sveltia CMS ' +
     '1.0. Use the `readonly` option instead.',
   save_all_locales:
     'The `save_all_locales` i18n option is deprecated and will be removed in Sveltia CMS 1.0. ' +
-    'Use the `initial_locales` option instead. ' +
+    'Use the `initial_locales` option instead. `save_all_locales: false` is equivalent to ' +
+    '`initial_locales: all`. ' +
     'https://github.com/sveltia/sveltia-cms#disabling-non-default-locale-content',
   automatic_deployments:
     'The `automatic_deployments` backend option is deprecated and will be removed in Sveltia CMS ' +
-    '1.0. Use the `skip_ci` option instead. Note that `skip_ci` takes an opposite value, meaning ' +
-    '`automatic_deployments: false` must be replaced with `skip_ci: true`. ' +
+    '1.0. Use the `skip_ci` option instead. `automatic_deployments: false` is equivalent to ' +
+    '`skip_ci: true`, and `automatic_deployments: true` is equivalent to `skip_ci: false`. ' +
     'https://github.com/sveltia/sveltia-cms#disabling-automatic-deployments',
 };
 
