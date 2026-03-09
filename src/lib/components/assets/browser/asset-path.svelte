81b3144f578f7854279e22685f9e7e83b5fefaf9
Kohei Yoshino <kohei.yoshino@gmail.com>
2026-03-09 11:45:52 -0400

    Handle empty asset paths in asset-path.svelte

--- src/lib/components/assets/browser/asset-path.svelte
+++ src/lib/components/assets/browser/asset-path.svelte
@@ -9,19 +9,20 @@
 
   /**
    * @typedef {object} Props
-   * @property {string} path The asset path to display.
+   * @property {string} path The asset path to display. It can be an empty string if no path is
+   * available (Lorem Picsum doesn’t provide image captions).
    * @property {Snippet} [children] Slot content.
    */
 
   /** @type {Props} */
   let {
     /* eslint-disable prefer-const */
-    path = '',
+    path,
     children = undefined,
     /* eslint-enable prefer-const */
   } = $props();
 
-  const pathArray = $derived(stripSlashes(path).split('/'));
+  const pathArray = $derived(path ? stripSlashes(path).split('/') : []);
   const sanitizeOptions = { ALLOWED_TAGS: ['wbr'] };
 
   /**
@@ -32,20 +33,22 @@
   const getLabel = (str) => sanitize(str.replace(/([-_.])/g, '$1<wbr>'), sanitizeOptions);
 </script>
 
-<span role="none" class="name">
-  <TruncatedText lines={2}>
-    {#each pathArray as segment, index (`${segment}-${index}`)}
-      {#if index === pathArray.length - 1}
-        <!-- File name -->
-        <strong>{@html getLabel(segment)}</strong>
-      {:else}
-        <!-- Folder name -->
-        {@html getLabel(segment)}/
-      {/if}
-    {/each}
-  </TruncatedText>
-  {@render children?.()}
-</span>
+{#if pathArray.length}
+  <span role="none" class="name">
+    <TruncatedText lines={2}>
+      {#each pathArray as segment, index (`${segment}-${index}`)}
+        {#if index === pathArray.length - 1}
+          <!-- File name -->
+          <strong>{@html getLabel(segment)}</strong>
+        {:else}
+          <!-- Folder name -->
+          {@html getLabel(segment)}/
+        {/if}
+      {/each}
+    </TruncatedText>
+    {@render children?.()}
+  </span>
+{/if}
 
 <style lang="scss">
   .name {
