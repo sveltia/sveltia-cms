2a9aa50de37a61184d9df7a39e5602bb74a5f1b1
Kohei Yoshino <kohei.yoshino@gmail.com>
2026-06-28 06:11:33 -0400

    Clarify registry API JSDoc references

--- src/lib/services/contents/api/registries.js
+++ src/lib/services/contents/api/registries.js
@@ -9,26 +9,31 @@
  */
 
 /**
- * Custom components registered using `CMS.registerEditorComponent`.
+ * Custom components registered using the `CMS.registerEditorComponent` API.
  * @type {Map<string, EditorComponentDefinition>}
+ * @see https://decapcms.org/docs/custom-widgets/#registereditorcomponent
+ * @see https://sveltiacms.app/en/docs/api/editor-components
  */
 export const customComponentRegistry = new Map();
 
 /**
+ * Custom file formats registered using the `CMS.registerFileFormat` API.
  * @type {Map<string, CustomFileFormat>}
+ * @see https://decapcms.org/docs/custom-formatters/
+ * @see https://sveltiacms.app/en/docs/api/file-formats
  */
 export const customFileFormatRegistry = new Map();
 
 /**
- * Custom entry preview stylesheet URLs registered with the `CMS.registerPreviewStyle()` API.
+ * Custom entry preview stylesheet URLs registered with the `CMS.registerPreviewStyle` API.
  * @type {Set<string>}
- * @see https://decapcms.org/docs/customization/
+ * @see https://decapcms.org/docs/customization/#registerpreviewstyle
  * @see https://sveltiacms.app/en/docs/api/preview-styles
  */
 export const customPreviewStyleRegistry = new Set();
 
 /**
- * Custom entry preview templates registered with the `CMS.registerPreviewTemplate()` API.
+ * Custom entry preview templates registered with the `CMS.registerPreviewTemplate` API.
  * @type {Map<string, ComponentType<CustomPreviewTemplateProps>>}
  * @see https://decapcms.org/docs/customization/#registerpreviewtemplate
  * @see https://sveltiacms.app/en/docs/api/preview-templates
@@ -36,6 +41,9 @@ export const customPreviewStyleRegistry = new Set();
 export const customPreviewTemplateRegistry = new Map();
 
 /**
+ * Custom event listeners registered with the `CMS.registerEventListener` API.
  * @type {Set<AppEventListener>}
+ * @see https://decapcms.org/docs/registering-events/
+ * @see https://sveltiacms.app/en/docs/api/events
  */
 export const eventHookRegistry = new Set();
